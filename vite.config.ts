844ffe34a747cec949dc44f0bfea9b358c785c4b
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2025-02-22 13:10:52 -0500

    update Tailwind CSS base color to silver, add vite-tsconfig-paths, and configure Vite for path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+//@ts-ignore
+import path from "path";
+
+//@ts-ignore
+const root = path.resolve(__dirname, "src");
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [tailwindcss(), tsconfigPaths(), react()],
+  resolve: {
+    alias: {
+      "@": root,
+    } as AliasOptions,
+  },
+});
